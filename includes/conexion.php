e0e10a2b544a75042ba5047799df19e8c3596d07
Kevin Santiago Prieto Guerrero <kspg2350@gmail.com>
2025-01-28 20:10:36 -0500

    conexion a la base de datos

--- includes/conexion.php
+++ includes/conexion.php
@@ -7,5 +7,8 @@ $pass = '';
 
 //manejo de errores
     try{
-        $pdo = new PDO('mysql:host=')
+        $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8',$user,$pass);
+        $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(Exception $e){
+        'error: ' .$e->getMessage();
     }
